dc3d3ba555fb7816078b31642c44d72370ec2d10
arafatqhassan-glitch <arafatqhassan@gmail.com>
2026-09-25 19:14:04 +0530

    Refactor directory creation methods in Utils

--- android/app/src/main/java/net/minetest/minetest/Utils.java
+++ android/app/src/main/java/net/minetest/minetest/Utils.java
@@ -9,46 +9,50 @@ import java.io.File;
 import java.util.Objects;
 
 public class Utils {
-	@NonNull
-	public static File createDirs(@NonNull File root, @NonNull String dir) {
-		File f = new File(root, dir);
-		if (!f.isDirectory())
-			if (!f.mkdirs())
-				Log.e("Utils", "Directory " + dir + " cannot be created");
+    private static final String TAG = "Utils";
 
-		return f;
-	}
+    @NonNull
+    public static File createDirs(@NonNull File root, @NonNull String dir) {
+        File f = new File(root, dir);
+        if (!f.isDirectory()) {
+            if (!f.mkdirs()) {
+                Log.e(TAG, "Directory " + f.getAbsolutePath() + " cannot be created");
+            }
+        }
+        return f;
+    }
 
-	@NonNull
-	public static File getUserDataDirectory(@NonNull Context context) {
-		// Custom path: /storage/emulated/0/ELI5
-		File customDir = new File(Environment.getExternalStorageDirectory(), "ELI5");
-		if (!customDir.exists()) {
-			if (!customDir.mkdirs()) {
-				Log.e("Utils", "Failed to create custom directory: " + customDir.getAbsolutePath());
-				// Fallback to standard app-specific external directory if custom creation fails
-				File extDir = context.getExternalFilesDir(null);
-				if (extDir != null) {
-					return createDirs(extDir, "ELI5");
-				}
-			}
-		}
-		return customDir;
-	}
+    @NonNull
+    public static File getUserDataDirectory(@NonNull Context context) {
+        // 1. Try public storage (/storage/emulated/0/ELI5)
+        File primaryDir = new File(Environment.getExternalStorageDirectory(), "ELI5");
+        if (primaryDir.exists() || primaryDir.mkdirs()) {
+            return primaryDir;
+        }
 
-	@NonNull
-	public static File getCacheDirectory(@NonNull Context context) {
-		return Objects.requireNonNull(
-			context.getCacheDir(),
-			"Cannot get cache directory"
-		);
-	}
+        // 2. Fallback to scoped app storage (Android/data/client.eli5.game/files/ELI5)
+        File fallbackDir = context.getExternalFilesDir(null);
+        if (fallbackDir != null) {
+            return createDirs(fallbackDir, "ELI5");
+        }
 
-	public static boolean isInstallValid(@NonNull Context context) {
-		File userDataDirectory = getUserDataDirectory(context);
-		return userDataDirectory.isDirectory() &&
-			new File(userDataDirectory, "builtin").isDirectory() &&
-			new File(userDataDirectory, "client").isDirectory() &&
-			new File(userDataDirectory, "textures").isDirectory();
-	}
+        // 3. Last resort internal storage
+        return createDirs(context.getFilesDir(), "ELI5");
+    }
+
+    @NonNull
+    public static File getCacheDirectory(@NonNull Context context) {
+        return Objects.requireNonNull(
+            context.getCacheDir(),
+            "Cannot get cache directory"
+        );
+    }
+
+    public static boolean isInstallValid(@NonNull Context context) {
+        File userDataDirectory = getUserDataDirectory(context);
+        return userDataDirectory.isDirectory() &&
+            new File(userDataDirectory, "builtin").isDirectory() &&
+            new File(userDataDirectory, "client").isDirectory() &&
+            new File(userDataDirectory, "textures").isDirectory();
+    }
 }
